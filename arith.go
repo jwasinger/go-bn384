c5f66a131eed4e4bc4688652d918b54574a7e1fa
Jared Wasinger <j-wasinger@hotmail.com>
2020-10-13 18:46:51 -0700

    don't use external uint128 lib

--- arith.go
+++ arith.go
@@ -2,11 +2,34 @@ package go_evm384
 
 import (
 	"math/bits"
-	"github.com/jwasinger/uint128"
 )
 
 const NUM_LIMBS = 6
 type Element [NUM_LIMBS]uint64
+
+// Cmp compares u and v and returns:
+//
+//   -1 if u <  v
+//    0 if u == v
+//   +1 if u >  v
+//
+func cmp128(u_hi, u_lo, v_hi, v_lo uint64) int {
+	if u_hi == v_hi && u_lo == v_lo {
+		return 0
+	} else if u_hi < v_hi || (u_hi == v_hi && u_lo < v_lo) {
+		return -1
+	} else {
+		return 1
+	}
+}
+
+// Add64 returns u+v.
+func Add64(u_hi, u_lo, v_hi, v_lo uint64) (uint64, uint64) {
+	lo, carry := bits.Add64(u_lo, v_lo, 0)
+	hi := u_hi + carry
+	return hi, lo
+}
+
 
 // out <- x + y
 func Add(out *Element, x *Element, y *Element) {
@@ -72,35 +95,35 @@ func SubMod(out *Element, x *Element, y *Element, mod *Element) {
 */
 func MulMod(out *Element, x *Element, y *Element, mod *Element, inv uint64) {
 	var A [NUM_LIMBS * 2 + 1]uint64
-	var xiyj, uimj, partial_sum, sum uint128.Uint128
-	var ui, carry uint64
-
-	xiyj = uint128.New(0, 0)
-	uimj = uint128.New(0, 0)
+	var xiyj_hi, xiyj_lo, uimj_hi, uimj_lo, partial_sum_hi, partial_sum_lo, sum_hi, sum_lo uint64 = 0, 0, 0, 0, 0, 0, 0, 0
+        var ui, carry uint64 = 0, 0
 
 	for i := 0; i < NUM_LIMBS; i++ {
 		ui = (A[i] + x[i] * y[0]) * inv
 
 		carry = 0
 		for j := 0; j < NUM_LIMBS; j++ {
-			xiyj.Lo = x[i]
-			xiyj.Hi = 0
+			xiyj_lo = x[i]
+			xiyj_hi, xiyj_lo = bits.Mul64(xiyj_lo, y[j])
 
-			xiyj = xiyj.Mul64(y[j])
+			uimj_lo = ui
+			uimj_hi = 0
 
-			uimj.Lo = ui
-			uimj.Hi = 0
+			uimj_hi, uimj_lo = bits.Mul64(uimj_lo, mod[j])
+			partial_sum_hi, partial_sum_lo = Add64(xiyj_hi, xiyj_lo, 0, carry)
 
-			uimj = uimj.Mul64(mod[j])
-
-			partial_sum = xiyj.Add64(carry)
+			/*
 			sum = uimj.Add64(A[i + j])
 			sum = sum.Add(partial_sum)
+			*/
 
-			A[i + j] = sum.Lo
-			carry = sum.Hi
+			sum_hi, sum_lo = Add64(uimj_hi, uimj_lo, 0, A[i + j])
+			sum_hi, sum_lo = Add64(sum_hi, sum_lo, partial_sum_hi, partial_sum_lo)
 
-			if sum.Cmp(partial_sum) == -1 {
+			A[i + j] = sum_lo
+			carry = sum_hi
+
+			if cmp128(sum_hi, sum_lo, partial_sum_hi, partial_sum_lo) == -1 {
 				var k int
 				k = 2
 				for ; i + j + k < NUM_LIMBS * 2 && A[i + j + k] == ^uint64(0); {
